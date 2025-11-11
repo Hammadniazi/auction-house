e3c0e3a74be52dd23bef0aad63f88d3237e71a41
Hammad-Niazi <hammadniazi991@gmail.com>
2025-11-11 13:40:33 +0100

    create a setup for the project and install the tailwind setup

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import "./style.css";
+
+document.querySelector("#app").innerHTML = `
+  <div class = "bg-blue-500">
+    Hllo Vite + TailwindCSS!
+  </div>
+`;
+
+setupCounter(document.querySelector("#counter"));
